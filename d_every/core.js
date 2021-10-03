2a08b87705546dfd54a070e7168ed3a6517045ed
Juli <julianmenav2@gmail.com>
2021-10-03 16:01:09 +0200

    Ejercicios every completados

--- d_every/core.js
+++ d_every/core.js
@@ -2,14 +2,14 @@
 // are even numbers.
 
 function allEven(input) {
-  return input;
+  return input.every((element) => element % 2 === 0);
 }
 
 // Check to see if all elements in an array
 // are of the same type.
 
 function allSameType(input) {
-  return input;
+  return input.every((element, i, arr) => typeof(element) === typeof(arr[0]));
 }
 
 // Check to see if every element in the matrix is
@@ -17,14 +17,14 @@ function allSameType(input) {
 // greater than 0.
 
 function positiveMatrix(input) {
-  return input;
+  return input.every((arr) => Array.isArray(arr) && arr.every((subelement) => subelement > 0))
 }
 
 // Check that all items in an array are strings
 // and that they all only contain the same vowels.
 
 function allSameVowels(input) {
-  return input;
+  return input.every((string) => typeof(string) === "string" && string.match(/[aeiou]/g).every((vowel) => vowel === string.match(/[aeiou]/g)[0]))
 }
 
 module.exports = {
